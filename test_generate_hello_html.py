828858209d0dbdda4daf7aa169e77764ec41baaf
nanthida92 <72985282+nanthida92@users.noreply.github.com>
2021-08-31 17:09:49 +0700

    Update test_generate_hello_html.py

--- test_generate_hello_html.py
+++ test_generate_hello_html.py
@@ -5,4 +5,5 @@ def test_hello_gihub_actions():
     assert generate_hello_html("") == "<span>Hello Github Actions!</span>"
 
 def test_hello_someone():
+    
     assert generate_hello_html("Athibet") == "<span>Hello Athibet!</span>"
